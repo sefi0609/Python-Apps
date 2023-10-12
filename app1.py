8eeb9a47225bcbe81146c7c9b1a52590830dc067
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-12 15:37:19 +0300

    full to-do app, need to fix exit

--- app1.py
+++ app1.py
@@ -1,10 +1,10 @@
-import utility
+FILE_PATH = 'todo_list.txt'
 
 
 def show():
     """ Returns the complete to-do ist as a string """
     prompt = ''
-    todos = utility.read_from_file()
+    todos = read_from_file()
     if len(todos) != 0:
         for index, line in enumerate(todos):
             prompt += f'{index + 1}. {line.title()}'
@@ -16,39 +16,59 @@ def show():
 
 def add(todo):
     """ Add a new to-do to the list """
-    todos = utility.read_from_file()
+    todos = read_from_file()
     todos.append(todo + '\n')
-    utility.write_to_file(todos)
+    write_to_file(todos)
     return f'The new item: "{todo}" was added to your todo-list'
 
 
 def edit(line_number, edit_line):
     """ Edit an existing line in the to-do list """
     # get the to-do list
-    todos = utility.read_from_file()
+    todos = read_from_file()
     todos[line_number - 1] = edit_line + '\n'
 
     # write the new list to the file
-    utility.write_to_file(todos)
+    write_to_file(todos)
     return f'The line number: {line_number}, has changed to "{edit_line}"'
 
 
 def complete(line_number):
     """ Complete a to-do list item """
-    todos = utility.read_from_file()
+    todos = read_from_file()
     # check if the number the user entered is ok
-    if check_number(line_number):
+    if is_valid_number(line_number):
         # drop the completed task
         complete_item = todos.pop(line_number - 1)
 
         # write the new list to the file
-        utility.write_to_file(todos)
+        write_to_file(todos)
         return f'The line: "{complete_item.strip()}" has completed successfully'
     else:
         return f'Not a valid line number,\nPlease enter a number between 1 and {len(todos)}'
 
 
-def check_number(line_number):
+def is_valid_number(line_number):
     """ Check if the number entered by the user is a valid line number in the to-do list """
-    todos = utility.read_from_file()
-    return utility.is_valid_number(len(todos), line_number)
+    todos = read_from_file()
+    number_of_lines = len(todos)
+    while True:
+        if number_of_lines >= line_number >= 1:
+            return True
+        else:
+            return False
+
+
+def write_to_file(todos, file_path=FILE_PATH):
+    """ Write the to-do list to a text file """
+    # delete the old file
+    with open(file_path, 'w') as file:
+        # write the new list to the file
+        file.writelines(todos)
+
+
+def read_from_file(file_path=FILE_PATH):
+    """ Read the to-do list from a text file """
+    with open(file_path, 'r') as file:
+        todos = file.readlines()
+    return todos
--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 import app1
 import PySimpleGUI as sg
 
+sg.theme('BlueMono')
+
 initial_prompt = """To edit or complete a todo list item,
 Please enter a line number and then press the matching button
 """
@@ -23,17 +25,16 @@ def todo():
             case 'Add':
                 output = app1.add(values['textinput'])
                 window['textbox'].update(output)
-                window['textinput'].update('')
             case 'Edit':
                 try:
                     # check if the number entered is a line in the to-do list
-                    if app1.check_number(int(values['textinput'])):
+                    if app1.is_valid_number(int(values['textinput'])):
                         # get the edited line
                         text = sg.popup_get_text('Enter The edited line', title="Waiting You Response")
-                        output = app1.edit(int(values['textinput']), text)
-                        # show the output
-                        window['textbox'].update(output)
-                        window['textinput'].update('')
+                        if text:
+                            output = app1.edit(int(values['textinput']), text)
+                            # show the output
+                            window['textbox'].update(output)
                     # if the number is not valid
                     else:
                         window['textbox'].update('Invalid number, Please try again')
@@ -44,9 +45,10 @@ def todo():
                 try:
                     output = app1.complete(int(values['textinput']))
                     window['textbox'].update(output)
-                    window['textinput'].update('')
                 except ValueError:
                     window['textbox'].update('Please enter a number: ')
+
+        window['textinput'].update('')
 
 
 if __name__ == '__main__':
