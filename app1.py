efd7afd4681bf0f6e2fa9bc0980c04e1dbcf7500
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-12 20:30:47 +0300

    changed the text box to list box, didn't finish

--- app1.py
+++ app1.py
@@ -1,19 +1,6 @@
 FILE_PATH = 'todo_list.txt'
 
 
-def show():
-    """ Returns the complete to-do ist as a string """
-    prompt = ''
-    todos = read_from_file()
-    if len(todos) != 0:
-        for index, line in enumerate(todos):
-            prompt += f'{index + 1}. {line.title()}'
-    else:
-        prompt = 'Your todo-list is empty, Please add things to do'
-
-    return prompt
-
-
 def add(todo):
     """ Add a new to-do to the list """
     todos = read_from_file()
@@ -22,7 +9,7 @@ def add(todo):
     return f'The new item: "{todo}" was added to your todo-list'
 
 
-def edit(line_number, edit_line):
+def edit(old_line, new_line):
     """ Edit an existing line in the to-do list """
     # get the to-do list
     todos = read_from_file()
@@ -33,19 +20,14 @@ def edit(line_number, edit_line):
     return f'The line number: {line_number}, has changed to "{edit_line}"'
 
 
-def complete(line_number):
+def complete(todo):
     """ Complete a to-do list item """
     todos = read_from_file()
-    # check if the number the user entered is ok
-    if is_valid_number(line_number):
-        # drop the completed task
-        complete_item = todos.pop(line_number - 1)
-
-        # write the new list to the file
-        write_to_file(todos)
-        return f'The line: "{complete_item.strip()}" has completed successfully'
-    else:
-        return f'Not a valid line number,\nPlease enter a number between 1 and {len(todos)}'
+    # drop the completed task
+    todos.remove(todo)
+    # write the new list to the file
+    write_to_file(todos)
+    return f'The line: "{todo.strip()}" has completed successfully'
 
 
 def is_valid_number(line_number):
--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import app1
-import time
 import PySimpleGUI as sg
+import time
+import threading
 
 sg.theme('BlueMono')
 
@@ -10,55 +11,38 @@ Please enter a line number and then press the matching button
 
 layout = [[sg.Text('', size=(10, 1), font=('Any', 25), key='timetext')],
           [sg.Text('Type in a to-do')],
-          [sg.InputText(size=42, tooltip='Enter todo: ', key='textinput')],
-          [sg.Multiline(initial_prompt, size=(40, 6), key='textbox')],
-          [sg.Button('Add'), sg.Button('Show'), sg.Button('Edit'), sg.Button('Complete'), sg.Button('Exit')]]
+          [sg.InputText(size=47, tooltip='Enter todo: ', key='textinput'), sg.Button('Add')],
+          [sg.Listbox(values=app1.read_from_file(), size=(45, 10), key='listbox'), sg.Button('Edit')],
+          [sg.Button('Complete'), sg.Button('Exit')]]
 
 
 def todo():
     """ Main entry point - deals with all the possible commends """
     window = sg.Window('My To-Do App', layout)
     while True:
-        event, values = window.read(timeout=10)
+        # clock configuration
+        event, values = window.read()
         window['timetext'].update(time.strftime('%H:%M:%S'))
         # two ways to exit the application
         if event in [sg.WINDOW_CLOSED, 'Exit']:
             break
         # handel the current user action
         match event:
-            case 'Show':
-                output = app1.show()
-                window['textbox'].update(output)
-                window['textinput'].update('')
             case 'Add':
                 if values['textinput'] != '':
+                    # need to release a thread for output popup
                     output = app1.add(values['textinput'])
-                    window['textbox'].update(output)
+                    window['listbox'].update(app1.read_from_file())
                     window['textinput'].update('')
             case 'Edit':
-                try:
-                    # check if the number entered is a line in the to-do list
-                    if app1.is_valid_number(int(values['textinput'])):
-                        # get the edited line
-                        text = sg.popup_get_text('Enter The edited line', title="Waiting You Response")
-                        if text:
-                            output = app1.edit(int(values['textinput']), text)
-                            # show the output
-                            window['textbox'].update(output)
-                    # if the number is not valid
-                    else:
-                        window['textbox'].update('Invalid number, Please try again')
-                # if the user didn't enter a number
-                except ValueError:
-                    window['textbox'].update('Please enter a number: ')
-                window['textinput'].update('')
+                text = sg.popup_get_text('Enter The edited line: ', title="Waiting You Response")
+                # need to release a thread for output popup
+                output = app1.edit(values['listbox'][0], text)
+                window['listbox'].update(app1.read_from_file())
             case 'Complete':
-                try:
-                    output = app1.complete(int(values['textinput']))
-                    window['textbox'].update(output)
-                except ValueError:
-                    window['textbox'].update('Please enter a number: ')
-                window['textinput'].update('')
+                # need to release a thread for output popup
+                output = app1.complete(values['listbox'][0])
+                window['listbox'].update(app1.read_from_file())
 
     window.close()
 
