e3624f41f403f30d94c4fd20bd48fcf2487cbb0b
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-15 12:31:45 +0300

    Add files via upload

---
+++ To-Do list App/web.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import app1
+import time
+
+
+def add_todo():
+    new_line = st.session_state['new_todo'].title() + '\n'
+    if new_line not in todos:
+        todos.append(new_line)
+        app1.write_to_file(todos)
+    else:
+        st.error('This todo is already in your list')
+    st.session_state['new_todo'] = ''
+
+
+st.title('My Todo App')
+st.subheader(time.strftime('%B %d %Y'))
+todos = app1.read_from_file()
+
+for todo in todos:
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.remove(todo)
+        app1.write_to_file(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label=' ', placeholder='Add new todo...',
+              on_change=add_todo, key='new_todo')
