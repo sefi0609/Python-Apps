56352020096549bf77caf12117aa62a6958f46fc
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-15 17:17:37 +0300

    Update todo.py

--- To-Do list web app/todo.py
+++ To-Do list web app/todo.py
@@ -4,8 +4,10 @@ import time
 
 
 def add_todo():
+    """ Add a new todo itme to the list """
     new_line = st.session_state['new_todo'].title() + '\n'
     st.session_state['new_todo'] = ''
+    
     if new_line not in todos:
         todos.append(new_line)
         app1.write_to_file(todos)
@@ -15,18 +17,23 @@ def add_todo():
 
 st.title('My Todo App')
 st.subheader(time.strftime('%B %d %Y'))
+# get the todos
 todos = app1.read_from_file()
 
+# create a checkbox for each todo
 for todo in todos:
     checkbox = st.checkbox(todo, key=todo)
+    # delete finished todos
     if checkbox:
         todos.remove(todo)
         app1.write_to_file(todos)
         del st.session_state[todo]
         st.rerun()
 
+# input a new todo
 st.text_input(label=' ', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
 
+# print an error message when the user tries to add the same task
 if app1.INDICATOR:
     st.error('This todo is already in your list')
     app1.INDICATOR = False
