64ded6af4a382134bc3d28c4331ca87ee891d852
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-21 12:50:15 +0300

    Update main.py

--- PDF Templates/main.py
+++ PDF Templates/main.py
@@ -6,6 +6,12 @@ df = pd.read_csv('topics.csv')
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 
+
+def create_multiple_lines():
+    for i in range(0, 270, 10):
+        pdf.line(10, i + 20, 210 - 10, i + 20)
+        
+
 for index, line in df.iterrows():
     pdf.add_page()
 
@@ -14,9 +20,7 @@ for index, line in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=line['Topic'], border=0, ln=1, align='L')
     
-    # create multiple lines
-    for i in range(0, 270, 10):
-        pdf.line(10, i + 20, 210 - 10, i + 20)
+    create_multiple_lines()
 
     # footer
     pdf.ln(264)
@@ -27,10 +31,8 @@ for index, line in df.iterrows():
     for page in range(line['Pages'] - 1):
         pdf.add_page()
 
-        # create multiple lines
-        for i in range(0, 270, 10):
-            pdf.line(10, i + 20, 210 - 10, i + 20)
-            
+        create_multiple_lines()
+        
         # footer
         pdf.ln(276)
         pdf.cell(0, 8, line['Topic'], align='R')
