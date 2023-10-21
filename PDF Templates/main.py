932b04a221e0f71e3bc3fb804c2fa10495bed497
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-10-21 12:45:34 +0300

    Update main.py

--- PDF Templates/main.py
+++ PDF Templates/main.py
@@ -13,8 +13,10 @@ for index, line in df.iterrows():
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=line['Topic'], border=0, ln=1, align='L')
-    pdf.set_line_width(0.4)
-    pdf.line(10, 20, 210-10, 20)
+    
+    # create multiple lines
+    for i in range(0, 270, 10):
+        pdf.line(10, i + 20, 210 - 10, i + 20)
 
     # footer
     pdf.ln(264)
@@ -24,8 +26,13 @@ for index, line in df.iterrows():
 
     for page in range(line['Pages'] - 1):
         pdf.add_page()
-        pdf.ln(276)
+
+        # create multiple lines
+        for i in range(0, 270, 10):
+            pdf.line(10, i + 20, 210 - 10, i + 20)
+            
         # footer
+        pdf.ln(276)
         pdf.cell(0, 8, line['Topic'], align='R')
 
 pdf.output('output.pdf')
