b2559cc2404ad3d350cf13e3dba89e49ca2ae33a
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-11-11 09:05:11 +0200

    Update backend.py

--- Weather Forecast/backend.py
+++ Weather Forecast/backend.py
@@ -4,7 +4,10 @@ from datetime import date, timedelta, datetime
 
 def get_data(place, days, option):
     # get the forecast from the API
-    url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={ENTER YOUR API KEY}'
+    url = f'https://api.openweathermap.org/data/2.5/forecast? \
+    q={place} \
+    &appid={ENTER YOUR API KEY}'
+    
     response = requests.get(url)
     data = response.json()
 
