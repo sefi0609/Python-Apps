295e2c4b67b50097861797d517cea3ac42893db3
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-12-13 12:19:11 +0200

    Update APOD.py

--- To-Do list web app/pages/APOD.py
+++ To-Do list web app/pages/APOD.py
@@ -8,14 +8,12 @@ data = response.json()
 
 # get the image and create an image file to display it
 image = requests.get(data['hdurl'])
-with open('today_image.jpg', 'wb') as file:
-    file.write(image.content)
 
 # display the title
 st.title(data['title'])
 
 # display the image
-st.image('today_image.jpg')
+st.image(image.content)
 
 # display the explanation
 st.write(data['explanation'])
